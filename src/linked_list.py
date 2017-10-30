83bea09b2063037607332c46d7bb79bc022418c4
Kinley Ramson <kinleyramson@gmail.com>
2017-10-29 19:28:56 -0700

    made the remove method

--- src/linked_list.py
+++ src/linked_list.py
@@ -4,9 +4,9 @@
 class Node(object):
     """Create a node class."""
 
-    def __init__(self, data=None, next=None):
+    def __init__(self, val=None, next=None):
         """Initialize each node instance."""
-        self.data = data
+        self.val = val
         self.next = next
 
 
@@ -47,6 +47,24 @@ class LinkedList(object):
                 return current_node
             else:
                 current_node = current_node.next
+
+    def remove(self, val):
+        """Remove node with given value from linked list."""
+        current_node = self.head
+        previous_node = None
+        flag = False
+        if current_node.val == val:
+            self.pop()
+            return
+        while current_node:
+            if current_node.val == val:
+                flag = True
+                previous_node.next = current_node.next
+            else:
+                previous_node = current_node
+                current_node = current_node.next
+        if not flag:
+            raise ValueError('That value is not in the list.')
 
     def __len__(self):
         """Return size of linked list when using len method."""
