3fb7eb5cdd4af667cc5cab93d4d1bbc03d829004
Kinley Ramson <kinleyramson@gmail.com>
2017-10-31 19:57:11 -0700

    teeeesssstttts

--- src/bin_heap.py
+++ src/bin_heap.py
@@ -34,9 +34,42 @@ class Heap(object):
             else:
                 return
 
+    def pop(self):
+        """Remove the top value of the heap."""
+        if self._size == 0:
+            raise ValueError("Empty: no value to pop.")
+        elif self._size < 3:
+            self._size -= 1
+            return self._heap_list.pop(0)
+        to_pop = self._heap_list[0]
+        new_head = self._heap_list[-1]
+        self._heap_list[-1], self._heap_list[0] = to_pop, new_head
+        self._heap_list.pop()
+        self._size -= 1
+        parent_index = 0
+        self._trickle_down(parent_index)
+        return to_pop
+
     def _parent(self, child_index):
         """Return parent index."""
         if child_index % 2:
             return (child_index - 1) // 2
         elif not child_index % 2:
             return (child_index - 2) // 2
+
+    def _children(self, parent_index):
+        """Return child index."""
+        child_list = []
+        if self._size > parent_index * 2 + 1:
+            child_list.append(parent_index * 2 + 1)
+        if self._size > parent_index * 2 + 2:
+            child_list.append(parent_index * 2 + 2)
+        return child_list
+
+    def _trickle_down(self, parent_index):
+        """Trickle value down to proper place in heap."""
+        children = self._children(parent_index)
+        for child in children:
+            if self._heap_list[parent_index] * self._multiplier < self._heap_list[child] * self._multiplier:
+                self._heap_list[parent_index], self._heap_list[child] = self._heap_list[child], self._heap_list[parent_index]
+                self._trickle_down(child)
